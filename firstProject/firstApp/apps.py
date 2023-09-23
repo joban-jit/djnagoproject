67422d7d3f1616252d3c3a9ae5f23c5d172b0d31
Jobanjit Singh <jbnjeet@gmail.com>
2023-09-23 15:26:04 +0530

    first project

---
+++ firstProject/firstApp/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+# to store app related configs
+
+class FirstappConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'firstApp'
