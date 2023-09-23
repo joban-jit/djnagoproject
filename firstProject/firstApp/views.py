4ed9b8a7e7a64e573d9237da4202756dc07b6155
Jobanjit Singh <jbnjeet@gmail.com>
2023-09-23 15:50:22 +0530

    first app

--- firstProject/firstApp/views.py
+++ firstProject/firstApp/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
-
+from django.http import HttpResponse
+import datetime
 # Create your views here.
+
+#function based views
+# it accepts request from browser and return http response
+def display(request):
+    return HttpResponse("<h1>My First Django APP!!!</h1>")
+
+def displayDateTime(request):
+    dt = datetime.datetime.now()
+    s = "<b>Current Date and Time:</b>"+str(dt)
+    return HttpResponse(s);
+#class based views
